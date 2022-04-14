d4c41a5f43060dbe85b5cd6b706e5da6b6018c2f
speria846 <niwenyesigasperia@gmail.com>
2022-04-14 09:33:25 +0300

    third commit

---
+++ src/main.kt
@@ -0,0 +1,44 @@
+fun main(){
+var result=currentAccount("A2324Uls","Speria",3563300)
+println( result.deposit(3400.0))
+println(result.withdraw(23432.0))
+    println(result.accountname)
+    println( result.accountnumber)
+   println( result.withdraw(2434.0))
+    println(result.withdraw(257.6))
+
+}
+//deposit(amount: Double) - Increments the balance bythe
+//amount deposited
+open class currentAccount(var accountnumber:String,var accountname:String,var balance:Int){
+fun deposit(amount:Double){
+    var amount=0.0
+    amount+=balance
+    amount++
+}
+    //withdraw(amount: Double) - Decrements the balanceby the
+    //amount withdraw
+fun withdraw(amount:Double){
+balance-amount
+  balance--
+}}
+//Prints out the account number and balanceand
+//name
+fun details(){
+
+}
+open class savingAccount(var withdraw:Int,accountnumber:String,accountname:String,balance:Int):currentAccount(accountnumber,accountname,balance){
+fun check(withdraw:String){
+var with=4
+    withdraw.forEach{a->a
+println(with)
+}
+}}
+//returns a string composed of only
+//the characters in even indices. For example given “banana” it will return
+fun returnString(name:String) {
+var name="instruments"
+    var n=name[0]+name[2].toStringname[4]
+}
+
+
